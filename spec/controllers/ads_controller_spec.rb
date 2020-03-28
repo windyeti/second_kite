1378e9fcfd7dfe821bb3b460df3006afe1f1a559
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2020-03-28 14:55:13 +0300

    add test controller ads index

--- spec/controllers/ads_controller_spec.rb
+++ spec/controllers/ads_controller_spec.rb
@@ -3,8 +3,19 @@ require 'rails_helper'
 RSpec.describe AdsController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
-      get :index
+    let(:ads) { create_list(:ad, 3, user: create(:user)) }
+
+    before { get :index }
+
+    it "render template index" do
+      expect(response).to render_template :index
+    end
+
+    it "assigns @ads" do
+      expect(assigns(:ads)).to eq ads
+    end
+
+    it "returns status success" do
       expect(response).to have_http_status(:success)
     end
   end
