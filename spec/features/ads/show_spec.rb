fdbce983cc741dad5f79543c0c438db6da2ac3f2
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2020-04-01 22:16:12 +0300

    add feature test Show ad

--- spec/features/ads/show_spec.rb
+++ spec/features/ads/show_spec.rb
@@ -1,1 +1,16 @@
+require 'rails_helper'
 
+feature 'User can see ad' do
+  let(:user) { create(:user) }
+  let!(:ad) { create(:ad, user: user) }
+
+  scenario 'Anybody can see ad' do
+    visit ads_path
+
+    within '.ad' do
+      click_on "#{ad.title}"
+    end
+
+    expect(page).to have_content ad.total_price
+  end
+end
