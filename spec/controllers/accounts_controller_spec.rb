fe5439481d7e61253b6677e5eeca158831c2e431
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2020-04-09 23:58:28 +0300

    test Guest user for account

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe AccountsController, type: :controller do
   describe 'GET #show' do
+    let(:user) { create(:user) }
     context 'Authenticate user can see his account' do
-      let(:user) { create(:user) }
       before { login(user) }
 
       it 'render template show' do
@@ -14,6 +14,18 @@ RSpec.describe AccountsController, type: :controller do
       it 'assigns account to var' do
         get :show, params: { id: user.account }
         expect(assigns(:account)).to eq user.account
+      end
+    end
+
+    describe 'Guest can not see account' do
+      it 'redirect to root' do
+        get :show, params: { id: user.account }
+        expect(response).to redirect_to new_user_session_path
+      end
+
+      it 'assigns account to var' do
+        get :show, params: { id: user.account }
+        expect(assigns(:account)).to be_nil
       end
     end
   end
