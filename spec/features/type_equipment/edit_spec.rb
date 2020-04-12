75d52887e1db6b812af0872eb122fe2f913caa0f
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2020-04-12 09:15:41 +0300

    add feature test type equipment edit_spec.rb

--- spec/features/type_equipment/edit_spec.rb
+++ spec/features/type_equipment/edit_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 feature 'User update type equipment' do
-  given!(:type_equipment_kite) { create(:type_equipment, name: 'Kite') }
+  given!(:type_equipment_kite) { create(:type_equipment, name: 'Old Name Kite') }
 
-  given(:admin_user) { create(:user, role: 'Admin') }
 
   context 'Admin' do
+    given(:admin_user) { create(:user, role: 'Admin') }
     background do
       sign_in(admin_user)
     end
@@ -19,19 +19,40 @@ feature 'User update type equipment' do
       fill_in 'Name', with: 'Super Kite'
 
       click_on 'Update Type equipment'
-save_and_open_page
 
       expect(page).to have_content 'Super Kite'
     end
 
-    context 'with valid data can update' do
+    scenario 'with invalid data can not update' do
+      visit root_path
+      click_on 'Type equipment'
 
-    end
-    context 'with invalid data can not update' do
+      click_on 'edit'
 
+      fill_in 'Name', with: ''
+
+      click_on 'Update Type equipment'
+
+      expect(page).to have_content 'Edit type equipment'
     end
   end
 
-  describe 'Authenticated user not admin'
-  describe 'Guest'
+  describe 'Authenticated user not admin' do
+    given(:user) { create(:user) }
+    background do
+      sign_in(user)
+    end
+    scenario 'can not update type equipment' do
+      visit root_path
+
+      expect(page).to_not have_content 'Type equipment'
+    end
+  end
+  describe 'Guest' do
+    scenario 'can not update type equipment' do
+      visit root_path
+
+      expect(page).to_not have_content 'Type equipment'
+    end
+  end
 end
