03e79de2f1f27cebf2b673838ad797fea2a4b34a
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2020-04-29 22:40:19 +0300

    stuff_name#create all tests feature and unit tests

--- spec/controllers/stuff_names_controller_spec.rb
+++ spec/controllers/stuff_names_controller_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe StuffNamesController, type: :controller do
   describe 'POST #create' do
+    let(:brand) { create(:brand) }
+
     context 'Admin' do
-      let(:brand) { create(:brand) }
       let(:admin_user) { create(:user, role: 'Admin') }
       before { login(admin_user) }
 
@@ -19,16 +20,74 @@ RSpec.describe StuffNamesController, type: :controller do
         it 'change stuff_name count' do
           expect do
             post :create, params: { brand_id: brand, stuff_name: attributes_for(:stuff_name) },format: :json
-          end.to change(StuffName, :count)
+          end.to change(StuffName, :count).by(1)
         end
         it 'return status ok' do
           post :create, params: { brand_id: brand, stuff_name: attributes_for(:stuff_name) },format: :json
           expect(response).to have_http_status :ok
         end
       end
-      context 'can not create stuff_name with invalid data'
+      context 'can not create stuff_name with invalid data' do
+        it 'assigns brand' do
+          post :create, params: { brand_id: brand, stuff_name: { name: '' } },format: :json
+          expect(assigns(:brand)).to eq brand
+        end
+        it 'assigns stuff_name' do
+          post :create, params: { brand_id: brand, stuff_name: { name: '' } },format: :json
+          expect(assigns(:stuff_name).valid?).to be_falsey
+        end
+        it 'does not change stuff_name count' do
+          expect do
+            post :create, params: { brand_id: brand, stuff_name: { name: '' } },format: :json
+          end.to_not change(StuffName, :count)
+        end
+        it 'return status unprocessable_entity' do
+          post :create, params: { brand_id: brand, stuff_name: { name: '' } },format: :json
+          expect(response).to have_http_status :unprocessable_entity
+        end
+      end
     end
-    context 'Authenticated user not admin'
-    context 'Guest'
+    context 'Authenticated user not admin' do
+      let(:user) { create(:user) }
+      before { login(user) }
+
+      it 'assigns brand' do
+        post :create, params: { brand_id: brand, stuff_name: attributes_for(:stuff_name) },format: :json
+        expect(assigns(:brand)).to eq brand
+      end
+      it 'assigns stuff_name' do
+        post :create, params: { brand_id: brand, stuff_name: attributes_for(:stuff_name) },format: :json
+        expect(assigns(:stuff_name)).to be_nil
+      end
+      it 'does not change stuff_name count' do
+        expect do
+          post :create, params: { brand_id: brand, stuff_name: attributes_for(:stuff_name) },format: :json
+        end.to_not change(StuffName, :count)
+      end
+      it 'return status forbidden' do
+        post :create, params: { brand_id: brand, stuff_name: attributes_for(:stuff_name) },format: :json
+        expect(response).to have_http_status :forbidden
+      end
+    end
+    context 'Guest' do
+
+      it 'assigns brand' do
+        post :create, params: { brand_id: brand, stuff_name: attributes_for(:stuff_name) },format: :json
+        expect(assigns(:brand)).to be_nil
+      end
+      it 'assigns stuff_name' do
+        post :create, params: { brand_id: brand, stuff_name: attributes_for(:stuff_name) },format: :json
+        expect(assigns(:stuff_name)).to be_nil
+      end
+      it 'does not change stuff_name count' do
+        expect do
+          post :create, params: { brand_id: brand, stuff_name: attributes_for(:stuff_name) },format: :json
+        end.to_not change(StuffName, :count)
+      end
+      it 'return status unauthorized' do
+        post :create, params: { brand_id: brand, stuff_name: attributes_for(:stuff_name) },format: :json
+        expect(response).to have_http_status :unauthorized
+      end
+    end
   end
 end
--- spec/features/stuff_name/create_spec.rb
+++ spec/features/stuff_name/create_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 feature 'Admin can create stuff_name', js: true do
+  given!(:brand) { create(:brand) }
+
   describe 'Admin' do
-    given!(:brand) { create(:brand) }
     given(:admin_user) { create(:user, role: 'Admin') }
     background { sign_in(admin_user) }
 
@@ -18,10 +19,36 @@ feature 'Admin can create stuff_name', js: true do
 
       expect(page).to have_content 'NewStuffName'
     end
-    scenario 'can not create stuff_name with invalid data'
+    scenario 'can not create stuff_name with invalid data' do
+      visit root_path
+      click_on 'Brands'
+      click_on brand.name
+
+      within '.stuff_name_form' do
+        fill_in 'stuff_name_name', with: ''
+        click_on 'Create Stuff name'
+      end
+
+      expect(page).to have_content 'Name can\'t be blank'
+      expect(page).to have_content 'New Stuff_name'
+    end
   end
-  describe 'Authenticated user not admin'
-    scenario 'can not create stuff_name'
-  describe 'Guest'
-    scenario 'can not create stuff_name'
+  describe 'Authenticated user not admin' do
+    given(:user) { create(:user) }
+    background { sign_in(user) }
+
+    scenario 'cannot create stuff_name' do
+      visit root_path
+
+      expect(page).to_not have_selector :link_or_button, 'Brands'
+    end
+
+  end
+  describe 'Guest' do
+    scenario 'cannot create stuff_name' do
+      visit root_path
+
+      expect(page).to_not have_selector :link_or_button, 'Brands'
+    end
+  end
 end
